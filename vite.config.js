afe54515fbe1ca912c35d42fdd36796978cb4b70
MichaelBreitung <mibreit@gmx.de>
2024-07-27 08:05:35 +0000

    minor: added cjs version

--- vite.config.js
+++ vite.config.js
@@ -28,6 +28,11 @@ export default defineConfig({
           preserveModulesRoot: 'src',
         },
         {
+          dir: 'lib-cjs',
+          format: 'cjs',
+          entryFileNames: `index.cjs`,
+        },
+        {
           dir: 'lib-iife',
           format: 'iife',
           name: 'mibreitDomTools',
